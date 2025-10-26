e854f0bb3cea45760c021d56039f3144f4bf7669
Zhouqi Jiang <luojia@openatom.club>
2025-10-26 13:37:46 +0800

    refactor: rename project crates into `robustone` and `robustone-cli`
    Signed-off-by: Zhouqi Jiang <luojia@openatom.club>

--- robustone-cli/src/utils/mod.rs
+++ robustone-cli/src/utils/mod.rs
@@ -1,6 +1,6 @@
 pub mod check;
 pub mod parse;
 
-// Re-export canonical entry points to keep `crate::cli::utils::*` working for older code.
+// Re-export canonical entry points to keep `crate::utils::*` working for older code.
 pub use check::validate_architecture;
 pub use parse::{parse_address, parse_hex_code};
