06d56e6bbe86390a88ea902cfc0a372d47eb7f3b
CAICAIIs <3360776475@qq.com>
2026-03-06 17:06:20 +0800

    refactor(core): extract riscv registration from robustone-core

--- robustone/src/lib.rs
+++ robustone/src/lib.rs
@@ -2,3 +2,12 @@
 
 #[doc(inline)]
 pub use robustone_core::*;
+
+#[doc(inline)]
+pub use robustone_riscv as riscv;
+
+pub fn dispatcher() -> ArchitectureDispatcher {
+    let mut dispatcher = ArchitectureDispatcher::new();
+    dispatcher.register(Box::new(riscv::RiscVHandler::new()));
+    dispatcher
+}
